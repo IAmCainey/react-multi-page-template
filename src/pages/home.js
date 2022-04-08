4cc38a24c7d8c339c98ff9b4b303c1e815d12198
Paul Caine <paulcaine@tutanota.com>
2022-04-08 05:06:53 +0100

    Added React Icons

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,18 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+// React Icons
+import { FaBeer } from "react-icons/fa";
+
 export default function HomePage() {
   return (
     <div className="container">
       <Helmet>
         <title>This is the title</title>
       </Helmet>
-      <h1>React Template</h1>
+      <h1>
+        <FaBeer className="dark" /> React Template
+      </h1>
       This is more for personal useage but please feel free to use.
     </div>
   );
